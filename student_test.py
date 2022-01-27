d4d8c6eb195ec3009e3c24530b6c8dfa8e15a6cd
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2022-01-27 13:30:28 -0500

    Update student_test.py

--- student_test.py
+++ student_test.py
@@ -4,7 +4,7 @@ import student
 
 
 def test_default():
-    input_values=['Power Glove', '1', '299.99']
+    input_values=['5.4']
     output=[]
 
     def mock_input(s=None):
@@ -20,24 +20,25 @@ def test_default():
 
     student.main()
 
-    assert output[3].lower()=='power glove has 1 in stock, and it costs $299.99', "It must be exact"
+    assert '5.4' in output[1] and '            5.4' in output[1] and '5.4         ' in output[1] and '005.4' in output[1]
+    
+ def test_verify():
+    input_values=['16.3']
+    output=[]
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+    
+    student.input = mock_input
+    student.print = lambda s : output.append(s)
+
+    student.main()
+
+    assert '16.3' in output[1] and '           16.3' in output[1] and '16.3        ' in output[1] and '016.3' in output[1]
  
-def test_two():
-    input_values=['blarg', '17', '1.99']
-    output=[]
-
-    def mock_input(s=None):
-        if s is not None:
-            output.append(s)
-            return input_values.pop(0)
-        else:
-            output.append("")
-            return input_values.pop(0)
-    
-    student.input = mock_input
-    student.print = lambda s : output.append(s)
-
-    student.main()
-
-    assert output[3].lower()=='blarg has 17 in stock, and it costs $1.99', "It must be exact"
 
